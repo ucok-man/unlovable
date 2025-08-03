e839bd7e097f0fdf39a0e31e95a6b9a44beeed99
Alvin Odhi N <ucokkocu411@gmail.com>
2025-08-04 06:28:46 +0700

    feat: setup e2b sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,22 +1,37 @@
-import { createAgent, openai } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
+// import { createAgent, openai } from "@inngest/agent-kit";
 import { inngest } from "./client";
+import { connectSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "test" },
   { event: "test" },
 
   async ({ event, step }) => {
-    const summarizer = createAgent({
-      name: "summarizer",
-      system:
-        "You are an expert summarizer. You summarize in just one sentence short!",
-      model: openai({ model: "gpt-4o-mini" }),
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("unlovable-nextjs");
+      return sandbox.sandboxId;
     });
 
-    const { output } = await summarizer.run(
-      `Summarize this: ${event.data.value}`
-    );
+    // const summarizer = createAgent({
+    //   name: "summarizer",
+    //   system:
+    //     "You are an expert summarizer. You summarize in just one sentence short!",
+    //   model: openai({ model: "gpt-4o-mini" }),
+    // });
 
-    return { output };
+    // const { output } = await summarizer.run(
+    //   `Summarize this: ${event.data.value}`
+    // );
+
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await connectSandbox({ sandboxId });
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
+    return { sandboxUrl };
+
+    // return { output };
   }
 );
